739758ccfcaa0994ff9cadc8a2b9ca2f08f4f7df
Grumblydude <Paskevic_Denis@web.de>
2023-03-07 11:20:51 +0100

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import React, {useState} from 'react'
+
+
+function Navbar() {
+  return (
+    <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    LOGO LINK
+                </Link>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
