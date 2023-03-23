2381cccb2f8d30213e78bba9a11f95eab19b6133
ERwashere <127199912+ERwashere@users.noreply.github.com>
2023-03-23 20:53:01 +0100

    Added Login Reworked dicctionary xml

---
+++ src/pages/LogIn.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { Card } from '@mui/material';
+
+
+export default function LogIn() {
+    return(
+        <Card className='LogIn'>
+            <CardContent>
+                <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                    <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                    <TextField id="input-with-sx" label="Name" variant="standard" />
+                </Box>
+
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
+                    <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                    <Input
+                        id="standard-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                        }
+                    />
+                </FormControl>
+
+                <Button 
+                    onClick={() => {
+                        alert('clicked');
+                    }}
+                    size="large"
+                    variant="contained">
+                    Log In
+                </Button>
+
+            </CardContent>
+        </Card>
+    )
+}
