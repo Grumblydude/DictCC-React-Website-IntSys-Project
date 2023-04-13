991e7edf998e9094938110392e7cdc7f876840f3
ERwashere <127199912+ERwashere@users.noreply.github.com>
2023-04-13 16:11:16 +0200

    Syncen

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,12 +2,21 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Card, Divider, Grid, Stack, Typography } from '@mui/material';
+import { Card, Divider, Grid, Stack, Typography, Button, IconButton, Dialog, DialogContent, DialogTitle, DialogActions, inputLabelClasses } from '@mui/material';
 import ToggleButton from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import { ModeEdit } from '@mui/icons-material';
+import { Icon } from '@mui/material';
+import { icons } from '@mui/icons-material';
+import { Container, height } from '@mui/system';
+import CardContent from '@mui/material/CardContent';
+import Profilekarte from './components/Profilekarte';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import { useState } from 'react';
+
+
 
 import './css/colors.module.css'
 import './css/typography.module.css'
@@ -16,17 +25,50 @@ import './css/tokens.css'
 
 export default function Profile() {
 
+
+
     const [editMode,toggleEditMode] = React.useState(false);
 
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'left',
-        color: theme.palette.text.secondary,
-        width: 202, 
-        height: 216
-      }));
+    const [cards, setCards] = useState([
+        { id: 1, name: 'Comes From' },
+        { id: 2, name: "Lives in" },
+        { id: 3, name: "Voting Power" },
+        { id: 4, name: "Contributions" },
+        { id: 5, name: "Speaks" },
+        { id: 6, name: "Speaks" },
+        { id: 7, name: "Speaks" },
+        { id: 8, name: "Total Score" },
+        { id: 9, name: "Member Since" }
+    ]);
+
+    const [open, setOpen] = useState(false); // dialog open state
+    const [newCardName, setNewCardName] = useState(""); // new card name state
+
+    const handleDeleteCard = (cardId) => {
+        const newCards = cards.filter((card) => card.id !== cardId);
+        setCards(newCards);
+    };
+
+    const handleClose = () => {
+        setOpen(false); // close the dialog
+        setNewCardName(""); // reset the new card name
+    };
+
+    const handleAddCard = () => {
+        setOpen(true); // open the dialog
+    };
+
+    const handleAddNewCard = () => {
+        const newId = cards.length + 1; // generate new id
+        const newCard = { id: newId, name: newCardName };
+        const newCards = [...cards, newCard];
+        setCards(newCards);
+        handleClose(); // close the dialog
+    };
+
+    const handleNewCardNameChange = (event) => {
+        setNewCardName(event.target.value); // update the new card name
+    };
 
     return(
         <React.Fragment>
@@ -69,32 +111,80 @@ export default function Profile() {
             <Divider variant='middle'/>
             <Grid
                 container
+                direction="row"
                 justifyContent="center"
                 alignItems="flex-start"
                 spacing={4}
                 padding={1}
-                >
-                {[
-                    'Comes From',
-                    'Lives In',
-                    'Voting Power',
-                    'Contributions',
-                    'Speaks',
-                    'Speaks',
-                    'Speaks',
-                    'Total Score',
-                    'Member since',
-                ].map((item) => (
-                    <Grid key={item} item xs={3} spacing={2} padding={2}>
-                        <Card sx={{width: 202, height: 216 }}>
-                            <Box sx={{ p: 2 }}>
-                                <Typography variant="body2">{item}</Typography>
-                            </Box>
-                        </Card>
+            >
+                {cards.map((card) => (
+                    <Grid item key={card.id}>
+                        <Profilekarte
+                            name={card.name}
+                            onDelete={() => handleDeleteCard(card.id)}
+                        />
                     </Grid>
                 ))}
-            </Grid>
+                <Grid item>
+                    <Card
+                        style={{ width: 202, height: 216 }}
+                        onClick={handleAddCard}
+                    >
+                        <CardContent
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                height: "100%",
+                            }}
+                        >
+                            <IconButton sx={{ fontSize: "3rem" }}>
+                                <AddCircleOutlineOutlinedIcon fontSize="inherit" />
+                            </IconButton>
+                            <Typography
+                                variant="h6"
+                                color= "#212427"
+                                sx={{ marginTop: 1, fontWeight: "bold" }}
+                            >
+                                Add Card
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+        </Grid>
+            
         </Stack>
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Add New Card</DialogTitle>
+            <DialogContent>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Card Name"
+                    fullWidth
+                    value={newCardName}
+                    onChange={handleNewCardNameChange}
+                    sx={{
+                        "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+                            borderColor: "orange",
+                        },
+                        }}
+                    InputLabelProps={{
+                        sx: {
+                            [`&.${inputLabelClasses.shrink}`]: {
+                                color: "#212427",
+                                
+                            },
+                        },
+                    }}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} style={{ color: '#212427' }}>Cancel</Button>
+                <Button onClick={handleAddNewCard} style={{ color: 'orange' }}>Finish</Button>
+            </DialogActions>
+            </Dialog>
         </React.Fragment>
     )
 }
